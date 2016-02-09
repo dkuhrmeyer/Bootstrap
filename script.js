8fadadd9a36c3015352f565d026bbd40c341215f
Maravis <markeezy00@gmail.com>
2016-02-08 17:27:13 -0700

    Created bootstrap alert for submit button

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
-    $('#myCarousel').carousel();
-})
+    //$('#myCarousel').carousel();
+    $('#registerAlert').hide();
+    $('#registerSubmitBtn').click(function(){
+        $('#registerAlert').show();
+    });
+});
